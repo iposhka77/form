fe1c7ad10bc76649ddf665fc1fb2f76c5479f5bd
iposhka77 <lolkek2150@gmail.com>
2024-12-18 18:20:30 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';  
-
 import FormComponent from './components/FormComponent';
+import FormList from './components/FormList'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
         <FormComponent />
+        <FormList /> 
       </header>
     </div>
   );
