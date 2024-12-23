c72866d6849e6110db2298fb6369958e6c991ae1
iposhka77 <lolkek2150@gmail.com>
2024-12-23 16:51:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';  
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import FormComponent from './components/FormComponent';
-import FormList from './components/FormList'; 
+import FormList from './components/FormList';
+import axios from 'axios';
 
 function App() {
+  const [forms, setForms] = useState([]);
+
+  const fetchForms = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/forms');
+      setForms(response.data);
+    } catch (error) {
+      console.error('Ошибка при получении анкет:', error);
+    }
+  };
+
+  const addForm = (newForm) => {
+    setForms((prevForms) => [...prevForms, newForm]); 
+  };
+
+  useEffect(() => {
+    fetchForms();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <FormComponent />
-        <FormList /> 
+        <FormComponent onFormSubmit={addForm} /> 
+        <FormList forms={forms} />
       </header>
     </div>
   );
